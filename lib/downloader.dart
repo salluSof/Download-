9d13b46a7de4995bde62a315c48496ef5c9ac9af
Mamun <sallusa514@gmail.com>
2024-07-03 11:31:44 +0600

    first commit

--- lib/downloader.dart
+++ lib/downloader.dart
@@ -1,15 +1,15 @@
 
 
 // import 'package:flutter_youtube_downloader/flutter_youtube_downloader.dart';
-//
+
 // class Download {
-//
-//   Future<void> downloadVideo( String youTube_link , String title  ) async {
-//     final result = await FlutterYoutubeDownloader.downloadVideo(
-//         youTube_link  , "$title", 18);
-//     print(result);
-//   }
-//
+
+ // Future<void> downloadVideo( String youTube_link , String title  ) async {
+  //   final result = await FlutterYoutubeDownloader.downloadVideo(
+    //    youTube_link  , "$title", 18);
+     print(result);
+ // }
+
 // }
 
 
